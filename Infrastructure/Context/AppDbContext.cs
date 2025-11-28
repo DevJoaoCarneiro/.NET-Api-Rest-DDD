0510176c57a7e944f7cd9da9a9f96e6cc3b6bdb6
Joao Victor <184165528+DevJoaoCarneiro@users.noreply.github.com>
2025-11-28 12:42:51 -0300

    Criação do Build Inicial API REST DDD + SOLID

---
+++ Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
